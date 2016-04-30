7faad3933e2e0c2cac5d8e2a26282403e710b073
Nathan Leite <natetheaverage@gmail.com>
2016-04-30 08:50:31 -0600

    adjusting dashboard script file

--- resources/views/includes/dash-scripts.blade.php
+++ resources/views/includes/dash-scripts.blade.php
@@ -1,24 +1,15 @@
 <!--JAVASCRIPT-->
 <!--=================================================-->
 
-<!--jQuery [ REQUIRED ]-->
-<script 
-	src="https://code.jquery.com/jquery-2.2.3.min.js" 
-	integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" 
-	crossorigin="anonymous"
-></script>
+<!-- <script src="/js/jquery.js"></script> -->
+<script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
 
-<script 
-	src="https://code.jquery.com/jquery-migrate-1.4.0.min.js" 
-	integrity="sha256-nxdiQ4FdTm28eUNNQIJz5JodTMCF5/l32g5LwfUwZUo=" 
-	crossorigin="anonymous"
-></script>
+<script src="https://code.jquery.com/jquery-migrate-1.4.0.min.js" integrity="sha256-nxdiQ4FdTm28eUNNQIJz5JodTMCF5/l32g5LwfUwZUo=" crossorigin="anonymous"></script>
+
+<script src="/js/bootstrap.min.js"></script>
 
 <!--Socket.io  [ Broadcasting server ] -->
 <script src="http://192.168.10.10:3000/socket.io/socket.io.js"></script>
-
-<!--BootstrapJS [ RECOMMENDED ]-->
-<script src="{!! asset('/js/bootstrap.min.js') !!}"></script>
 
 <!--Fast Click [ OPTIONAL ]-->
 <script src="{!! asset('/plugins/fast-click/fastclick.min.js') !!}"></script>
